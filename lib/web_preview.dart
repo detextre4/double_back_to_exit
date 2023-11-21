edd5f1df58cb60b6246211861a23ffbec996d9f9
detextre4 <detextre4@gmail.com>
2023-11-21 13:53:15 -0400

    fixing snackbarMessage

--- lib/web_preview.dart
+++ lib/web_preview.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class DoubleBackToExitWidget extends StatelessWidget {
-  const DoubleBackToExitWidget({super.key, arMessage, required this.child});
+  const DoubleBackToExitWidget({
+    super.key,
+    required this.snackBarMessage,
+    required this.child,
+  });
+  final String snackBarMessage;
   final Widget child;
 
   @override
